722cca3ab0fd72fc45504c1ea21ba88bfce0fefd
Henrique Fittipaldi <henrique.gfitti@gmail.com>
2020-10-23 17:06:39 -0300

    total

--- convert_json_to_csv.py
+++ convert_json_to_csv.py
@@ -15,4 +15,6 @@ with open(f_csv, 'w', newline='') as file:
     csv_file = csv.writer(file, delimiter=(','))
     csv_file.writerow(['ID', 'name', 'description', 'quantity', 'value', 'total'])
     for i in data['order']:
-        csv_file.writerow([i['id'], i['name'], i['description'], i['quantity'], i['value'], total])
+        csv_file.writerow([i['id'], i['name'], i['description'], i['quantity'], i['value'], (i['quantity'] * i['value'])])
+    csv_file.writerow(['Total', '    ', '    ', '    ', '    ', total])
+

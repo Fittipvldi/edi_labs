7e6e46f4fbe504989d4d879dfda523cda7018231
Henrique Fittipaldi <henrique.gfitti@gmail.com>
2020-10-27 20:29:21 -0300

    'total_json'

--- convert_csv_to_json.py
+++ convert_csv_to_json.py
@@ -1,4 +1,8 @@
 import json, csv
+from convert_json_to_csv import total
+
+with open('./json/shopping.json') as file:
+    data_json = json.load(file)
 
 f_csv = './gerados/shopping_answer.csv'
 f_json = './gerados/shopping_answer.json'
@@ -7,7 +11,12 @@ with open(f_csv, 'r') as file:
     csv_file = csv.DictReader(file)
     data = {'order': []}
     for i in csv_file:
-        data['order'].append(i)
+        if(i['ID'] == 'Total'):
+            pass
+        else:
+            data['order'].append(i)
+
+    data['total'] = total(data_json)
 
 with open(f_json, 'w') as file:
     file.write(json.dumps(data, indent=4))
--- convert_json_to_csv.py
+++ convert_json_to_csv.py
@@ -4,17 +4,21 @@ with open('./json/shopping.json') as file:
     data = json.load(file)
 
 f_csv = './gerados/shopping_answer.csv'
-total = 0
 
-for i in data['order']:
-    quantity = i['quantity']
-    value = i['value']
-    total += value * quantity
+
+def total(data):
+    total = 0
+
+    for i in data['order']:
+        quantity = i['quantity']
+        value = i['value']
+        total += value * quantity
+    return total
+
 
 with open(f_csv, 'w', newline='') as file:
     csv_file = csv.writer(file, delimiter=(','))
     csv_file.writerow(['ID', 'name', 'description', 'quantity', 'value', 'total'])
     for i in data['order']:
         csv_file.writerow([i['id'], i['name'], i['description'], i['quantity'], i['value'], (i['quantity'] * i['value'])])
-    csv_file.writerow(['Total', '    ', '    ', '    ', '    ', total])
-
+    csv_file.writerow(['Total', '    ', '    ', '    ', '    ', total(data)])
